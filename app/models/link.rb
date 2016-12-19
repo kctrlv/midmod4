917d5176a82e0ec215dc5ef6fdd4f137192a9f73
David Davydov <kctrlv@gmail.com>
2016-12-19 13:45:24 -0700

    displays only users links

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,9 +5,11 @@ class Link < ActiveRecord::Base
   validate :url_is_valid
 
   def url_is_valid
-    uri = URI.parse(url)
-    unless uri.kind_of?(URI::HTTP) or uri.kind_of?(URI::HTTPS)
-      errors.add(:url, "is not a valid URL")
+    if url
+      uri = URI.parse(url)
+      unless uri.kind_of?(URI::HTTP) or uri.kind_of?(URI::HTTPS)
+        errors.add(:url, "is not a valid URL")
+      end
     end
   end
 end
