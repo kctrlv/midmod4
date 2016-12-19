381c0c171ebf4d77ccb5e396809209fb64e3503e
David Davydov <kctrlv@gmail.com>
2016-12-19 16:22:38 -0700

    hotread url update to heroku

--- app/services/hot_read_service.rb
+++ app/services/hot_read_service.rb
@@ -1,7 +1,7 @@
 class HotReadService
   def self.read(link)
     link.update_attribute(:read, true)
-    conn = Faraday.new(url: 'http://localhost:2000')
+    conn = Faraday.new(url: 'http://midmod-hotreads.herokuapp.com')
     conn.post('/api/v1/reads', {url: link.url} )
   end
 end
