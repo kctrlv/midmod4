dab43240d9a6a3bf2c22067e29bfb0852f68799e
David Davydov <kctrlv@gmail.com>
2016-12-19 13:50:52 -0700

    model specs on link

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Link, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :url }
+
+  it 'validates that the url is a valid url' do
+    link = Link.new(title: "Something", url: "bla")
+    link.save
+    expect(link.errors.messages[:url]).to eq(["is not a valid URL"])
+  end
 end
