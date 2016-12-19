66622de150862ef95d4d8fdf0fe4f109a7b54c67
David Davydov <kctrlv@gmail.com>
2016-12-19 13:42:04 -0700

    sad path specs for submitting links

--- spec/features/user_submits_links_spec.rb
+++ spec/features/user_submits_links_spec.rb
@@ -10,4 +10,19 @@ RSpec.feature "User submits links", :type => :feature do
       expect(page).to have_link("Cool link")
     end
   end
+
+  scenario "User submits invalid link and gets error" do
+    login_as_bob
+    fill_in "Title", with: "Cool link"
+    fill_in "Url", with: "invalid"
+    click_button "Submit"
+    expect(page).to have_content("Url is not a valid URL")
+  end
+
+  scenario "User submits link without title" do
+    login_as_bob
+    fill_in "Url", with: "https://stallman.org/"
+    click_button "Submit"
+    expect(page).to have_content("Title can't be blank")
+  end
 end
